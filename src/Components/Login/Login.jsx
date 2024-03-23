82c4ee70f7a9fa4c8d0b4da56beb7524fa59c483
ahmed ashraf <andahmed371@gmail.com>
2024-03-23 23:04:40 +0200

    agggah

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,13 +1,13 @@
 export default function Login() {
-    return (
-        <div className="grid grid-cols-1 items-center justify-items-center h-screen">
-            < div >
-                <h1>Welcome Back</h1>
-                <p>Sign in to Your Account</p>
-            </ div>
-            <form className="flex flex-col gap-4">
-                <input type="text" />
-            </form>
-        </div >
-    )
+  return (
+    <div className="grid grid-cols-1 items-center justify-items-center h-screen">
+      <div>
+        <h1>Welcome Back</h1>
+        <p>Sign in to Your Accountsdfghjkjhgfdg</p>
+      </div>
+      <form className="flex flex-col gap-4">
+        <input type="text" />
+      </form>
+    </div>
+  );
 }
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,8 +1,7 @@
-
 export default function Register() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <p>fgjhkl;knmbvcfyuioknbgfyui</p>
+    </div>
+  );
 }
