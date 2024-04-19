776dfef0f7fd8800473f8c14d42e7d906b2fbe61
ahmed ashraf <andahmed371@gmail.com>
2024-04-19 18:22:40 +0200

    nothing changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import Main from "./Components/Main/Main";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./Components/Register/Register";
-import Login from './Components/Login/Login'
+import Login from "./Components/Login/Login";
 import IntroPage from "./Components/IntroPage/IntroPage";
 import { useState } from "react";
 
-
 export default function App() {
-
   const [userAuth, setUserAuth] = useState(true);
 
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={userAuth ? <Main /> : <IntroPage />}></Route>
+          <Route path="/" element={userAuth ? <Main /> : <IntroPage />}></Route>
           <Route path="/register" element={<Register />}></Route>
           <Route path="/login" element={<Login />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
