5bec760cadd05cc1c41e9d4c3580979159268ba7
Ahmed Algrgawy <ahmedalgrgawy77@gmail.com>
2024-04-22 18:19:59 +0200

    Resposive for Newsletters is Done

--- src/Components/Home/Newsletters/Newsletters.jsx
+++ src/Components/Home/Newsletters/Newsletters.jsx
@@ -3,16 +3,16 @@ import "./Newsletters.css"
 export const Newsletters = () => {
     return (
         <div>
-            <div className="mt-[40px] mx-0 py-[60px] px-[80px] flex justify-between flex-wrap items-center bg-[#222]" id="newsletter">
+            <div className="mt-[40px] py-[40px] px-[20px] mx-0 md:py-[60px] md:px-[80px] flex justify-between flex-wrap items-center bg-[#222]" id="newsletter">
                 <div>
                     <h4 className="text-[22px] font-[700] text-white">Sign Up For Newsletters</h4>
                     <p className="text-[14px] font-[600] text-secondary">Get E-mail Updates About Our<span className="text-main"> Latest Shop & Special
                         Offers</span></p>
                 </div>
-                <div className="flex w-[40%]">
-                    <input className="h-[50px] py-0 px-[20px] text-[14px] w-full outline-none"
+                <div className="flex w-full lg:w-[40%] md:w-[70%]">
+                    <input className="h-[50px] py-0 px-[20px] text-[14px] w-full outline-none mt-3 lg:mt-0"
                         type="text" placeholder="Your Email Address" />
-                    <button className="bg-main text-white whitespace-nowrap px-4">Sign Up</button>
+                    <button className="bg-main text-white whitespace-nowrap px-4 mt-3 lg:mt-0">Sign Up</button>
                 </div>
             </div>
         </div>
