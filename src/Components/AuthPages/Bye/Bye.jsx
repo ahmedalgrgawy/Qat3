871e62ccdf5783d7934ce1947beeb3b04c7f4801
Ahmed Algrgawy <ahmedalgrgawy77@gmail.com>
2024-04-24 22:19:36 +0200

    Bye Bye Page

--- src/Components/AuthPages/Bye/Bye.jsx
+++ src/Components/AuthPages/Bye/Bye.jsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom"
+import { FaRegSadCry } from "react-icons/fa";
 
 const Bye = () => {
     return (
         <div className='h-screen flex justify-center items-center flex-col'>
-            <div className="text-red-600 text-6xl mb-10">Visit Us Again!</div>
-            <div>
+            <div className="text-main text-7xl mb-10">{<FaRegSadCry />}</div>
+            <div className="text-main-darker text-3xl md:text-6xl mb-10">Come Back Again</div>
+            <div className="flex flex-col justify-center gap-10">
                 <Link className="main-btn" to={"/sign-in"}>
-                    Login again
+                    Login Again
+                </Link>
+                <Link className="second-btn" to={"/"}>
+                    Get Back
                 </Link>
             </div>
         </div>
