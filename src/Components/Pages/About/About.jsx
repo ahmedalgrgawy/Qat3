c1949d4eceaf2224508842df32e97697f5ecee0c
ZOCKS0 <zelkhamary1@gmail.com>
2024-04-30 06:40:31 +0300

    About us and single product added still need update

--- src/Components/Pages/About/About.jsx
+++ src/Components/Pages/About/About.jsx
@@ -1,3 +1,5 @@
+import pic1 from "../../../assets/landing-image.png"
+
 
 export default function About() {
     return (
@@ -5,9 +7,29 @@ export default function About() {
             <h2 className="main-title my-28">
                 About Us
             </h2>
-            <div>
-                about
+            <div className="container grid grid-cols-2 gap-4 flex items-center mb-20">
+                <div className="">
+                <img src={pic1} alt="" />
+
+                </div>
+                <div className=" p-10">
+                    <h2 className="text-5xl font-bold mb-5">Who We Are ?</h2>
+
+                    <p className="text-gray-600 mb-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima, provident. Aliquid commodi repudiandae recusandae quam at voluptatum nobis distinctio. Ipsa qui doloribus officiis natus aliquam, reiciendis nulla totam sapiente aperiam molestias, sed facere, iure perspiciatis sunt autem earum neque possimus. Incidunt nostrum laboriosam saepe doloremque unde et quisquam minus maxime officiis pariatur quo alias nemo iste corporis, autem sint, quod facilis, dolorum rerum! Possimus quidem, accusantium quibusdam itaque laudantium a sunt dicta, soluta tenetur at eius, reprehenderit neque dolorum id.</p>
+                
+                <p className="text-gray-400 mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae id dicta officia et magni. Consequuntur, earum sint. Reiciendis, possimus quis.</p>
+                
+                <marquee className="bg-gray-400" behavior="smoath" direction="left">"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae id dicta officia et magni. Consequuntur, earum sint. Reiciendis, possimus quis. "</marquee>
+                </div>
+
             </div>
+
+                <div className="container grid grid-cols-1 gap-4 flex items-center my-10 text-center" >
+<h1 className="text-6xl font-bold">Download Our <a className="cursor-pointer underline decoration-5 text-sky-800">App</a></h1>
+
+    <img src={pic1} alt="" className="w-3/5 m-auto" />
+
+                </div>
         </div>
     )
 }
--- src/Components/Pages/SingleProduct/SingleProduct.jsx
+++ src/Components/Pages/SingleProduct/SingleProduct.jsx
@@ -1,6 +1,11 @@
 import { FaCircle } from "react-icons/fa6";
 import { FaShoppingCart } from "react-icons/fa"
 import product from "../../../assets/f5.jpg"
+import pic1 from "../../../assets/product-pictures/picture1.jpg"
+import pic2 from "../../../assets/product-pictures/picture2.jpg"
+import pic3 from "../../../assets/product-pictures/picture3.jpg"
+import pic4 from "../../../assets/product-pictures/picture4.jpg"
+
 export default function SingleProduct() {
 
 
@@ -9,9 +14,64 @@ export default function SingleProduct() {
             <h2 className="main-title my-28">
                 productName
             </h2>
+
+
+
             <div className="container">
-                <div className="single">
-                    اشتغل هنا يا خماري
+                <div className="single grid grid-cols-2 gap-5 flex items-center">
+                <div className=" text-center">
+
+                <img src={pic1} alt="main-picture" className="mb-5 w-3/5 m-auto" />
+
+                    <div className="grid grid-cols-4 gap-2 ">
+                    <img src={pic1} alt="first-picture" />
+                    <img src={pic2} alt="second-picture" />
+                    <img src={pic3} alt="third-picture" />
+                    <img src={pic4} alt="forth-picture" />
+
+                    </div>
+
+                </div>
+
+<div className="">
+<p className="mb-5 text-base text-black-900">Home / T-shirt</p>
+
+<h4 className="text-3xl font-semibold">Men's Fashion T-Shirt</h4>
+
+<h2 className=" my-5 text-3xl font-bold">$ 140,50</h2>
+
+
+
+
+<div className="my-10">
+
+<select className="me-5 p-2 border-2 rounded-xl " name="size" id="">
+
+    <option selected disabled value="none">Select Size</option>
+    <option value="small">Small</option>
+    <option value="medium">Medium</option>
+    <option value="large">Large</option>
+    <option value="XLarge">XL</option>
+    <option value="2XLarge">XXL</option>
+
+</select>
+
+<input type="number" min="1" className="mx-5 p-2 w-14 border-2 border-gray rounded-xl" />
+
+<button className="ms-5 py-2 px-5 bg-yellow-500 text-white font-semibold rounded-full shadow-md hover:bg-yellow-400 focus:outline-none focus:ring focus:ring-violet-500 focus:ring-opacity-75">Add To Cart</button>
+</div>
+
+<h2 className="mb-5 text-3xl font-bold">Product Details :</h2>
+
+<p className="text-lg text-black-900">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi molestias veritatis dignissimos aut maxime nam tempore quo atque, cumque ut fugit reiciendis asperiores, inventore, quidem natus reprehenderit mollitia incidunt? Adipisci aut velit voluptas ad soluta magni dolorem, cumque molestiae vero totam maxime laborum? Corrupti, officia deserunt? Reprehenderit velit sequi harum? Praesentium ad ut accusamus voluptates deleniti labore repudiandae nulla exercitationem?
+</p>
+</div>
+
+
+
+
+
                 </div>
                 <div className="similar text-center">
                     <h2 className="main-title my-28">
