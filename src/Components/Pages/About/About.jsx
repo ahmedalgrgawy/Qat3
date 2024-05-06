77addb0b3dac93f9567352c435f88c11b5532317
ZOCKS0 <zelkhamary1@gmail.com>
2024-05-06 22:26:55 +0300

    responsive about and single page done + video

--- src/Components/Pages/About/About.jsx
+++ src/Components/Pages/About/About.jsx
@@ -1,4 +1,4 @@
-import App from "../../../App";
+import App from "../../../assets/App Video/App.mp4";
 import { logos } from "../../../assets/data";
 
 export default function About() {
@@ -7,11 +7,11 @@ export default function About() {
             <h2 className="main-title my-28">
                 About Us
             </h2>
-            <div className="container flex justify-around gap-10 items-center mb-20 mx-auto">
-                <div className="w-[30%]">
+            <div className="container flex-wrap flex justify-around items-center mb-20 mx-auto">
+                <div className="md:w-[100%] lg:w-[35%] mx-auto">
                     <img src={logos.logoOne} alt="Qat3" />
                 </div>
-                <div className=" w-[65%] p-10">
+                <div className=" md:w-[100%] lg:w-[65%] p-10">
                     <h2 className="text-5xl font-bold mb-5">Why Choose Qat3?</h2>
 
                     <p className="text-gray-600 mb-5">We partner with a wide range of Egyptian vendors, from established brands to exciting new startups. By shopping with Qat3, you're directly supporting the local economy and helping Egyptian businesses thrive, We offer a diverse selection of products across various categories, ensuring you can find everything you need, from fashion and homeware to electronics and groceries.</p>
@@ -26,8 +26,8 @@ export default function About() {
             <div className="container flex flex-col items-center justify-center my-10 text-center" >
                 <h1 className="text-4xl font-bold my-5">Download Our <a className="cursor-pointer underline decoration-5 text-sky-800">App</a></h1>
 
-                <div className="w-[30%]">
-                    <video loop autoPlay src={App.video} alt="App" className="mx-auto" />
+                <div className="w-[75%] lg:w-[50%]">
+                    <video loop autoPlay src={App} alt="App" className=" w-[100%]" />
                 </div>
 
             </div>
